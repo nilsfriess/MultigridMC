b1dbc0a08516c307fdf420d48af187262b5fa884
Eike Mueller <eike.h.mueller@gmail.com>
2023-04-24 15:48:10 +0100

    Added first implementation of multigrid MC sampler

---
+++ src/sampler/multigridmc_sampler.cc
@@ -0,0 +1,86 @@
+#include "multigridmc_sampler.hh"
+/** @file multigridmc_sampler.cc
+ *
+ * @brief Implementation of multigridmc_sampler.hh
+ */
+
+/** Create a new instance */
+MultigridMCSampler::MultigridMCSampler(std::shared_ptr<LinearOperator> linear_operator_,
+                                       std::mt19937_64 &rng_,
+                                       const MultigridMCParameters params_,
+                                       std::shared_ptr<SamplerFactory> presampler_factory_,
+                                       std::shared_ptr<SamplerFactory> postsampler_factory_,
+                                       std::shared_ptr<IntergridOperatorFactory> intergrid_operator_factory_,
+                                       std::shared_ptr<SamplerFactory> coarse_sampler_factory_) : Sampler(linear_operator_, rng_),
+                                                                                                  params(params_),
+                                                                                                  presampler_factory(presampler_factory_),
+                                                                                                  postsampler_factory(postsampler_factory_),
+                                                                                                  intergrid_operator_factory(intergrid_operator_factory_),
+                                                                                                  coarse_sampler_factory(coarse_sampler_factory_)
+{
+    // Extract underlying fine lattice
+    std::shared_ptr<Lattice> lattice = linear_operator->get_lattice();
+    // Linear operator on a given level
+    std::shared_ptr<LinearOperator> lin_op = linear_operator;
+    for (int level = 0; level < params.nlevel; ++level)
+    {
+        x_ell.push_back(Eigen::VectorXd(lattice->M));
+        f_ell.push_back(Eigen::VectorXd(lattice->M));
+        r_ell.push_back(Eigen::VectorXd(lattice->M));
+        linear_operators.push_back(lin_op);
+        std::shared_ptr<Sampler> presampler = presampler_factory->get(lin_op);
+        presamplers.push_back(presampler);
+        std::shared_ptr<Sampler> postsampler = postsampler_factory->get(lin_op);
+        postsamplers.push_back(postsampler);
+        if (level < params.nlevel - 1)
+        {
+            std::shared_ptr<IntergridOperator> intergrid_operator = intergrid_operator_factory->get(lattice);
+            intergrid_operators.push_back(intergrid_operator);
+            lin_op = std::make_shared<LinearOperator>(lin_op->coarsen(intergrid_operator));
+            //  Move to next-coarser lattice
+            lattice = lattice->get_coarse_lattice();
+        }
+    }
+    coarse_sampler = coarse_sampler_factory->get(lin_op);
+}
+
+/** Recursive solve on a given level */
+void MultigridMCSampler::sample(const unsigned int level)
+{
+    if (level == params.nlevel - 1)
+    {
+        // Coarse level solve
+        coarse_sampler->apply(f_ell[level], x_ell[level]);
+    }
+    else
+    {
+        // Presampler
+        for (unsigned int k = 0; k < params.npresample; ++k)
+        {
+            presamplers[level]->apply(f_ell[level], x_ell[level]);
+        }
+        // Compute residual
+        linear_operators[level]->apply(x_ell[level], r_ell[level]);
+        r_ell[level] = f_ell[level] - r_ell[level];
+        intergrid_operators[level]->restrict(r_ell[level], f_ell[level + 1]);
+        // Recursive call
+        x_ell[level + 1].setZero();
+        sample(level + 1);
+        // Prolongate and add
+        intergrid_operators[level]->prolongate_add(x_ell[level + 1], x_ell[level]);
+        // Postsmooth
+        for (unsigned int k = 0; k < params.npostsample; ++k)
+        {
+            postsamplers[level]->apply(f_ell[level], x_ell[level]);
+        }
+    }
+}
+
+/** Solve the linear system Ax = b with one iteration of the multigrid V-cycle */
+void MultigridMCSampler::apply(const Eigen::VectorXd &f, Eigen::VectorXd &x)
+{
+    f_ell[0] = f;
+    x_ell[0] = x;
+    sample(0);
+    x = x_ell[0];
+}
---
+++ src/sampler/multigridmc_sampler.hh
@@ -0,0 +1,93 @@
+#ifndef MULTIGRIDMC_SAMPLER_HH
+#define MULTIGRIDMC_SAMPLER_HH MULTIGRIDMC_SAMPLER_HH
+#include <memory>
+#include <vector>
+#include <Eigen/Dense>
+#include "linear_operator/linear_operator.hh"
+#include "intergrid/intergrid_operator.hh"
+#include "sampler.hh"
+
+/** @file multigridmc_sampler.hh
+ *
+ * @brief Multigrid Monte Carlo sampler
+ */
+
+/** @struct Multigrid Monte Carlo parameters */
+struct MultigridMCParameters
+{
+    /** @brief Number of levels */
+    unsigned int nlevel;
+    /** @brief Number of presmoothing steps */
+    unsigned int npresample;
+    /** @brief number of postsmoothing steps */
+    unsigned int npostsample;
+};
+
+/** @class MultigridMCSampler
+ *
+ * @brief Sampler based on Multigrid Monte Carlo
+ */
+class MultigridMCSampler : public Sampler
+{
+public:
+    /** @brief Create a new instance
+     *
+     * @param[in] linear_operator_ underlying linear operator
+     * @param[in] rng_ random number generator
+     * @param[in] params_ multigrid Monte Carlo parameters
+     * @param[in] presampler_factory_ factory for presampler on each level
+     * @param[in] postsampler_factory_ factory for postsampler on each level
+     * @param[in] intergrid_operator_factory_ factory for intergrid operators on each level
+     * @param[in] coarse_sampler_factory_ factory for coarse solver
+     */
+    MultigridMCSampler(std::shared_ptr<LinearOperator> linear_operator_,
+                       std::mt19937_64 &rng_,
+                       const MultigridMCParameters params_,
+                       std::shared_ptr<SamplerFactory> presampler_factory_,
+                       std::shared_ptr<SamplerFactory> postsampler_factory_,
+                       std::shared_ptr<IntergridOperatorFactory> intergrid_operator_factory_,
+                       std::shared_ptr<SamplerFactory> coarse_sampler_factory_);
+
+    /** @brief Solve the linear system Ax = b
+     *
+     * @param[in] b right hand side b
+     * @param[out] x solution x
+     */
+    virtual void apply(const Eigen::VectorXd &b, Eigen::VectorXd &x);
+
+protected:
+    /** @brief Recursive solve on a givel level
+     *
+     * @param[in] level level on which to solve recursively
+     */
+    void sample(const unsigned int level);
+
+    /** @brief parameters */
+    const MultigridMCParameters params;
+    /** @brief presampler factory on each level */
+    std::shared_ptr<SamplerFactory> presampler_factory;
+    /** @brief postsmoother factory on each level */
+    std::shared_ptr<SamplerFactory> postsampler_factory;
+    /** @brief intergrid operator factory on each level */
+    std::shared_ptr<IntergridOperatorFactory> intergrid_operator_factory;
+    /** @brief factory for coarse sampler */
+    std::shared_ptr<SamplerFactory> coarse_sampler_factory;
+    /** @brief coarse level solver */
+    std::shared_ptr<Sampler> coarse_sampler;
+    /** @brief linear operators on all levels */
+    std::vector<std::shared_ptr<LinearOperator>> linear_operators;
+    /** @brief smoothers on all levels */
+    std::vector<std::shared_ptr<Sampler>> presamplers;
+    /** @brief smoothers on all levels */
+    std::vector<std::shared_ptr<Sampler>> postsamplers;
+    /** @brief intergrid operators on all levels (except the coarsest) */
+    std::vector<std::shared_ptr<IntergridOperator>> intergrid_operators;
+    /** @brief Solution on each level */
+    std::vector<Eigen::VectorXd> x_ell;
+    /** @brief RHS on each level */
+    std::vector<Eigen::VectorXd> f_ell;
+    /** @brief Residual on each level */
+    std::vector<Eigen::VectorXd> r_ell;
+};
+
+#endif // MULTIGRIDMC_SAMPLER_HH
