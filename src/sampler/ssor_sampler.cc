d94b153665cccf19be3e67a066d496291f0ab2d8
Eike Mueller <eike.h.mueller@gmail.com>
2023-05-13 10:17:45 +0100

    Bugfix: moved implementation to correct file

--- src/sampler/ssor_sampler.cc
+++ src/sampler/ssor_sampler.cc
@@ -5,28 +5,6 @@
 
 #include "ssor_sampler.hh"
 
-/* apply Sampler */
-void SORSampler::apply(const Eigen::VectorXd &f, Eigen::VectorXd &x) const
-{
-    // Diagonal part
-    for (unsigned int ell = 0; ell < c_rhs.size(); ++ell)
-    {
-        double tmp = sqrt_precision_diag[ell];
-        c_rhs[ell] = tmp * normal_dist(rng) + f[ell];
-    }
-    // low-rank correction to covariance matrix
-    if (linear_operator->get_m_lowrank() > 0)
-    {
-        const LinearOperator::DenseMatrixType B = linear_operator->get_B();
-        for (unsigned int ell = 0; ell < xi.size(); ++ell)
-        {
-            xi[ell] = normal_dist(rng);
-        }
-        c_rhs += B * (*U_lowrank) * xi;
-    }
-    smoother->apply(c_rhs, x);
-}
-
 /** apply SSOR sampler */
 void SSORSampler::apply(const Eigen::VectorXd &f, Eigen::VectorXd &x) const
 {
