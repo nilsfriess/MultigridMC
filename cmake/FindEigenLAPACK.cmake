46ad204355d497b1a6c4e5012a5a57fe59072cd4
eikehmueller <e.mueller@bath.ac.uk>
2023-07-05 19:53:32 +0100

    Try to compile LAPACKE code first before trying to find library explicitly

--- cmake/FindEigenLAPACK.cmake
+++ cmake/FindEigenLAPACK.cmake
@@ -17,16 +17,30 @@ if(NOT LAPACK_FOUND)
 else()
 endif()
 
-# on MACOS we need to explicitly find the LAPACKE library
-find_library(LAPACKE_LIBRARY lapacke)
+# Try to compile lapacke code
+check_c_source_compiles("
+#include <lapacke.h> 
+int main(int argc, char **argv) {}
+" LAPACKE_COMPILES)
 
-if(LAPACKE_LIBRARY)
-    message(STATUS "Found LAPACKE: " ${LAPACKE_LIBRARY})
-    link_libraries(${LAPACKE_LIBRARY})
-else()
-    message(ERROR " Cound not find LAPACKE library")
-    set(LAPACK_FOUND false)
+check_c_source_compiles("
+#include <mkl_lapacke.h> 
+int main(int argc, char **argv) {}
+" MKL_LAPACKE_COMPILES)
+
+
+if(NOT (LAPACKE_COMPILES OR MKL_LAPACKE_COMPILES))
+    message(STATUS " Cannot compile LAPACKE code - trying to find LAPACKE library.")
+    find_library(LAPACKE_LIBRARY lapacke)
+    if(LAPACKE_LIBRARY)
+        message(STATUS "Found LAPACKE: " ${LAPACKE_LIBRARY})
+        link_libraries(${LAPACKE_LIBRARY})
+    else()
+        message(ERROR " Could not find LAPACKE library")
+        set(LAPACK_FOUND false)
+    endif()
 endif()
+
 
 # Set up Eigen with BLAS/LAPACK support
 if(BLAS_FOUND AND LAPACK_FOUND)
