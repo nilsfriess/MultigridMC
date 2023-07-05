f4e40e0f82b9fc161fcb2ba8fe5719999b298305
eikehmueller <eike.h.mueller@gmail.com>
2023-07-05 11:29:31 +0200

    Improved logic for finding BLAS/LAPACK, moved to a separate file

---
+++ cmake/FindEigenLAPACK.cmake
@@ -0,0 +1,42 @@
+# Find BLAS/LAPACK for the Eigen library
+#
+
+# find BLAS
+find_package(BLAS REQUIRED)
+
+if(NOT BLAS_FOUND)
+    message(ERROR "Could not find BLAS library!")
+else()
+endif()
+
+# find LAPACK
+find_package(LAPACK REQUIRED)
+
+if(NOT LAPACK_FOUND)
+    message(ERROR "Could not find LAPACK library!")
+else()
+endif()
+
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
+    # on MACOS we need to explicitly find the LAPACKE library
+    find_library(LAPACKE_LIBRARY lapacke)
+
+    if(LAPACKE_LIBRARY)
+        message(STATUS "Found LAPACKE: " ${LAPACKE_LIBRARY})
+        link_libraries(${LAPACKE_LIBRARY})
+    else()
+        message(ERROR "Cound not find LAPACKE library")
+        set(LAPACK_FOUND false)
+    endif()
+endif()
+
+# Set up Eigen with BLAS/LAPACK support
+if(BLAS_FOUND AND LAPACK_FOUND)
+    message(STATUS "Using Eigen with BLAS/LAPACK support.")
+    link_libraries(BLAS::BLAS)
+    link_libraries(LAPACK::LAPACK)
+    set(EIGEN_USE_BLAS "true")
+    set(EIGEN_USE_LAPACKE "true")
+else()
+    message(WARNING "Falling back to Eigen without BLAS/LAPACK support.")
+endif()
