8cf3c585a1d2a3cfa1ff04f80c211561ac8fc8bd
Eike Mueller <e.mueller@bath.ac.uk>
2023-07-28 09:50:54 +0200

    Print number of vertices, since these now determine the number of dofs in the FEM discretisation

--- src/lattice/lattice2d.cc
+++ src/lattice/lattice2d.cc
@@ -9,6 +9,6 @@ std::string Lattice2d::get_info() const
 {
     const int buffersize = 80;
     char buffer[buffersize];
-    snprintf(buffer, buffersize, "2d lattice, %4d x %4d points, %4d unknowns", nx, ny, Ncell);
+    snprintf(buffer, buffersize, "2d lattice, %4d x %4d points, %4d unknowns", nx, ny, Nvertex);
     return std::string(buffer);
 }
--- src/lattice/lattice3d.cc
+++ src/lattice/lattice3d.cc
@@ -9,6 +9,6 @@ std::string Lattice3d::get_info() const
 {
     const int buffersize = 80;
     char buffer[buffersize];
-    snprintf(buffer, buffersize, "3d lattice, %4d x %4d x %4d points, %4d unknowns", nx, ny, nz, Ncell);
+    snprintf(buffer, buffersize, "3d lattice, %4d x %4d x %4d points, %4d unknowns", nx, ny, nz, Nvertex);
     return std::string(buffer);
 }
